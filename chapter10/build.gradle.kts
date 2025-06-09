6021b08c721515d52fdff96cce0fb6bddb06fda4
인철환(2230204) <cheolhwan.ihn@hanwha.com>
2025-06-09 16:23:38 +0900

    [chapter10] schema registry docker-compose.yml 파일 반영

---
+++ chapter10/build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    kotlin("jvm") version "1.9.25"
+    kotlin("plugin.spring") version "1.9.25"
+    kotlin("plugin.serialization") version "1.9.25"
+    id("org.springframework.boot") version "3.5.0"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.study"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // kafka schema
+    // avro-4k 를 사용하기 위해서는 버전확인 필수(https://github.com/avro-kotlin/avro4k)
+    implementation("com.github.avro-kotlin.avro4k:avro4k-core:${avro4kVersion}")
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.kafka:spring-kafka")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
+    developmentOnly("org.springframework.boot:spring-boot-docker-compose")
+    runtimeOnly("com.h2database:h2")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("org.springframework.kafka:spring-kafka-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ chapter10/src/main/kotlin/com/study/chapter10/config/KafkaConfig.kt
@@ -0,0 +1,54 @@
+package com.study.chapter10.config
+
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.kafka.common.serialization.StringSerializer
+import org.springframework.context.annotation.Bean
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
+import org.springframework.kafka.core.ConsumerFactory
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory
+import org.springframework.kafka.core.DefaultKafkaProducerFactory
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.core.ProducerFactory
+
+class KafkaConfig {
+
+    @Bean
+    fun producerFactory(): ProducerFactory<String, String> =
+        DefaultKafkaProducerFactory(
+            mapOf(
+                // 필수로 작성 필요 bootStrapServer, Serializer
+                ProducerConfig.BOOTSTRAP_SERVERS_CONFIG to "localhost:19092",
+                ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG to StringSerializer::class.java,
+                ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG to KafkaAvroDeserializer::class.java,
+
+                ProducerConfig.ACKS_CONFIG to "1", // ack = 0, 1, all
+                ProducerConfig.RETRIES_CONFIG to 10, // 재시도
+            )
+        )
+
+    @Bean
+    fun kafkaTemplate(): KafkaTemplate<String, String> = KafkaTemplate(producerFactory())
+
+    @Bean
+    fun consumerFactory(): ConsumerFactory<String, String> =
+        DefaultKafkaConsumerFactory(
+            mapOf(
+                ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG to "localhost:19092",
+                ConsumerConfig.GROUP_ID_CONFIG to "test-group",
+                ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG to StringDeserializer::class.java,
+                ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to StringDeserializer::class.java,
+                // ConsumerConfig.AUTO_OFFSET_RESET_CONFIG to "earliest",
+
+                // 격리 수준
+                ConsumerConfig.DEFAULT_ISOLATION_LEVEL to "read_committed",
+            )
+        )
+
+    @Bean
+    fun kafkaListenerContainerFactory(): ConcurrentKafkaListenerContainerFactory<String, String> =
+        ConcurrentKafkaListenerContainerFactory<String, String>().apply {
+            consumerFactory = consumerFactory()
+        }
+}
